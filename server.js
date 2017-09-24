439cf6f983e55b6d3c7c62b8a73900b7b50d2195
danilo.carrabino <danilo.carrabino@gmail.com>
2017-09-25 00:18:32 +0200

    Separated routing and core logic; started templating in ejs

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 // grab our dependencies
 const express = require('express'),
-  app = express(),
-  port = process.env.PORT || 8080;
+    app = express(),
+    port = process.env.PORT || 8080
+    expressLayouts = require('express-ejs-layouts');
 
 // configure our application
 
+// tell express where to look for static assets
+app.use(express.static(__dirname + '/public'));
+
+// set ejs as templating engine
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
 // set the routes
-app.get('/', (req, res) => {
-  res.send('Hello, I am the app!');
-});
+app.use(require('./app/routes'));
 
 // start our server
 app.listen(port, () => {
-  console.log(`App listening on http://localhost:${port}`);
+    console.log(`App listening on http://localhost:${port}`);
 });
