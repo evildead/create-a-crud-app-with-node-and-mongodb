7c2d4c9ec05061564641ea9de3785f0c7d6588aa
danilo.carrabino <danilo.carrabino@gmail.com>
2017-09-27 16:55:38 +0200

    Before introducing the Mongoose models

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,22 @@
 // create a new express Router
 const express = require('express'),
     router = express.Router(),
-    mainController = require('./controllers/main.controller');
+    mainController = require('./controllers/main.controller'),
+    eventsController = require('./controllers/events.controller');
 
 // export router
 module.exports = router;
 
 // define routes
+// main routes
 router.get('/', mainController.showHome);
+
+// events routes
+router.get('/events', eventsController.showEvents);
+router.get('/events/:slug', eventsController.showSingle);
+
+// create events
+
+// edit events
+
+// delete events
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+// load environment variables
+require('dotenv').config();
+
 // grab our dependencies
 const express = require('express'),
     app = express(),
     port = process.env.PORT || 8080
-    expressLayouts = require('express-ejs-layouts');
+    expressLayouts = require('express-ejs-layouts'),
+    mongoose = require('mongoose');
 
 // configure our application
 
@@ -12,6 +16,9 @@ app.use(express.static(__dirname + '/public'));
 // set ejs as templating engine
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
+
+// connect to the database
+mongoose.connect(process.env.DB_URI);
 
 // set the routes
 app.use(require('./app/routes'));
