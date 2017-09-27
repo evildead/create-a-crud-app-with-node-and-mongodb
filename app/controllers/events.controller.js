feb2ad978eede7191a2e7ccf0bc342142cb6da01
danilo.carrabino <danilo.carrabino@gmail.com>
2017-09-27 18:19:54 +0200

    Database seeded with Mongoose

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -1,12 +1,9 @@
+const Event = require('../models/event');
+
 module.exports = {
     // show all events
     showEvents: (req, res) => {
-        // create dummy events
-        const events = [
-            { name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.' },
-            { name: 'Swimming', slug: 'swimming', description: 'Michael Phelps is the fast fish.' },
-            { name: 'Weightlifting', slug: 'weightlifting', description: 'Lifting heavy things up' }
-        ];
+        // get all the events
 
         // return a view with data
         res.render('pages/events', {events: events});
@@ -18,5 +15,28 @@ module.exports = {
 
         // return a view with data
         res.render('pages/single', {event: event});
+    },
+
+    // seed our database
+    seedEvents: (req, res) => {
+        // create dummy events
+        const events = [
+            { name: 'Basketball', description: 'Throwing into a basket.' },
+            { name: 'Swimming', description: 'Michael Phelps is the fast fish.' },
+            { name: 'Weightlifting', description: 'Lifting heavy things up' },
+            { name: 'Ping Pong', description: 'Super fast paddles' }
+        ];
+
+        // use the Event model to insert/save
+        Event.remove({}, () => {
+            // After removing all the events present in the database, perform the seeding!
+            for(event of events) {
+                var newEvent = new Event(event);
+                newEvent.save();
+            }
+        });
+
+        // seeded!
+        res.send('Database seeded!');
     }
 };
