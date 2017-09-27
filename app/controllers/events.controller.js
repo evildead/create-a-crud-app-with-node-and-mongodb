6b26c9bb7faab47ea2fe05fa09a364b3aa148e99
danilo.carrabino <danilo.carrabino@gmail.com>
2017-09-27 18:46:07 +0200

    Show all events and single event from slug param

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -1,42 +1,95 @@
 const Event = require('../models/event');
 
 module.exports = {
-    // show all events
-    showEvents: (req, res) => {
-        // get all the events
+    showEvents: showEvents,
+    showSingle: showSingle,
+    seedEvents: seedEvents
+}
 
+/**
+ * Show all the events
+ * @param {request} req 
+ * @param {response} res 
+ */
+function showEvents(req, res) {
+    // get all the events
+    Event.find({}, (err, events) => {
+        // error found
+        if(err) {
+            res.status(404);
+            res.send('Events not found');
+        }
+
+        // no error
         // return a view with data
-        res.render('pages/events', {events: events});
-    },
-
-    showSingle: (req, res) => {
-        // get a single event
-        const event = { name: 'Basketball', slug: 'basketball', description: 'Throwing into a basket.' };
-
-        // return a view with data
-        res.render('pages/single', {event: event});
-    },
-
-    // seed our database
-    seedEvents: (req, res) => {
-        // create dummy events
-        const events = [
-            { name: 'Basketball', description: 'Throwing into a basket.' },
-            { name: 'Swimming', description: 'Michael Phelps is the fast fish.' },
-            { name: 'Weightlifting', description: 'Lifting heavy things up' },
-            { name: 'Ping Pong', description: 'Super fast paddles' }
-        ];
-
-        // use the Event model to insert/save
-        Event.remove({}, () => {
-            // After removing all the events present in the database, perform the seeding!
-            for(event of events) {
-                var newEvent = new Event(event);
-                newEvent.save();
-            }
+        res.render('pages/events', {
+            events: events
         });
+    });
+}
 
-        // seeded!
-        res.send('Database seeded!');
-    }
-};
+/**
+ * Show a single event
+ * @param {request} req 
+ * @param {response} res 
+ */
+function showSingle(req, res) {
+    // get a single event
+    const event = {
+        name: 'Basketball',
+        slug: 'basketball',
+        description: 'Throwing into a basket.'
+    };
+
+    Event.findOne({slug: req.params.slug}, (err, event) => {
+        // error found
+        if(err) {
+            res.status(404);
+            res.send('Events not found');
+        }
+
+        // no error
+        // return a view with data
+        res.render('pages/single', {
+            event: event
+        });
+    });
+}
+
+/**
+ * Seed the database
+ * @param {request} req 
+ * @param {response} res 
+ */
+function seedEvents(req, res) {
+    // create dummy events
+    const events = [{
+            name: 'Basketball',
+            description: 'Throwing into a basket.'
+        },
+        {
+            name: 'Swimming',
+            description: 'Michael Phelps is the fast fish.'
+        },
+        {
+            name: 'Weightlifting',
+            description: 'Lifting heavy things up'
+        },
+        {
+            name: 'Ping Pong',
+            description: 'Super fast paddles'
+        }
+    ];
+
+    // use the Event model to insert/save
+    Event.remove({}, () => {
+        // After removing all the events present in the database, perform the seeding!
+        for (event of events) {
+            var newEvent = new Event(event);
+            newEvent.save();
+        }
+    });
+
+    // seeded!
+    res.send('Database seeded!');
+}
